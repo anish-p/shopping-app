65fe3ff1bd66481a036266e85c2bc8a4db535a46
Rama Krishnan Shankar <ramakrishnan@Ramakrishnan-MacBook-Pro.local>
2017-08-17 10:48:39 +0530

    Login

--- src/main/java/com/tw/shoppingapp/controller/UserLoginController.java
+++ src/main/java/com/tw/shoppingapp/controller/UserLoginController.java
@@ -3,16 +3,19 @@ package com.tw.shoppingapp.controller;
 import com.tw.shoppingapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
 
+@RestController
 public class UserLoginController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public boolean Login(String username, String password){
+        System.out.println("sadfjasd" + username + password);
         if(userService.getUser(username, password)!=null)
             return true;
         return false;
